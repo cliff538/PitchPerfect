95662bb8bfe8a3b3220910891403049fcd20f1e9
cliff538 <cliff538@yahoo.com>
2015-01-06 22:41:12 -0700

    Working good at this point.

--- PlaySoundsViewController.swift
+++ PlaySoundsViewController.swift
@@ -10,35 +10,42 @@ import UIKit
 import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	
+	var audioPlayer:AVAudioPlayer!   // audioPlayer is being declared here.
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+			var filePathUrl = NSURL.fileURLWithPath(filePath)
+			audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil) // audioPlayer initilized here.
 			
-			if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
-					var filePathUrl = NSURL.fileURLWithPath(filePath)
-				
-			} else {
-				println("The FilePath is empty")
-			}
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-	@IBOutlet weak var playSlowAudio: UIButton!
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+		} else {
+			println("The FilePath is empty")
+		}
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+		
+	@IBAction func playSlowAudio(sender: UIButton) {
+		
+		audioPlayer.play() // audioPlayer used here.
+	}
+	
+	/*
+	// MARK: - Navigation
+	
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+	// Get the new view controller using segue.destinationViewController.
+	// Pass the selected object to the new view controller.
+	}
+	*/
+	
 }
